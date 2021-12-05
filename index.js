8d2909aef21b0338fe29d24399dbeee2f14fd6ce
Basteque <82060824+Basteque@users.noreply.github.com>
2021-12-05 12:56:46 +0100

    Horizontal Cards + js

---
+++ index.js
@@ -0,0 +1,9 @@
+function navPortfolio() {
+    window.location = "test.html";
+}
+
+var pf = document.querySelector(".Left")
+var cv = document.querySelector(".Center");
+var am = document.querySelector(".Right");
+
+pf.addEventListener('click', navPortfolio);
