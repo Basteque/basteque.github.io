f282e2c45fe2cfcf5a74d3c79b9bbb588a7b5126
Basteque <82060824+Basteque@users.noreply.github.com>
2021-12-07 21:48:25 +0100

    js links

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ function navPortfolio() {
     window.location = "test.html";
 }
 
-var pf = document.querySelector(".Left")
-var cv = document.querySelector(".Center");
-var am = document.querySelector(".Right");
+function navCV() {
+    window.location = "image/CV.png";
+}
+
+function navAbout() {
+    window.location = "https://www.linkedin.com/in/basile-m-22bb3baa/";
+}
+
+var pf = document.querySelector(".Left h1")
+var cv = document.querySelector(".Center h1");
+var am = document.querySelector(".Right h1");
 
 pf.addEventListener('click', navPortfolio);
+am.addEventListener('click', navAbout);
+cv.addEventListener('click', navCV);
